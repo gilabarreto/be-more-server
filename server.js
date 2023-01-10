6fa8bd770c9c78aa87281bb0e23c755ef289ee9c
Victor Gila Barreto <gilabarreto@gmail.com>
2023-01-10 09:52:35 -0700

    Make get request to chatGPT

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+require('dotenv').config()
+
 app.get('/', (req, res) => {
   res.send("Hello")
 })
+
+
+app.get('/chat', async (req, res) => {
+
+  async function fetchData() {
+    try {
+      const response = await axios.post(
+        'https://api.openai.com/v1/completions',
+        {
+          'model': 'text-davinci-002',
+          'prompt': 'Tell me a joke.',
+          'temperature': 1,
+          'max_tokens': 1024
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': process.env.REACT_APP_CHATGPT_KEY
+          },
+        },
+      );
+
+      console.log(response.data.choices[0].text);
+      return response.data.choices[0].text;
+      
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  const joke = await fetchData()
+  res.send(joke);
+});
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
